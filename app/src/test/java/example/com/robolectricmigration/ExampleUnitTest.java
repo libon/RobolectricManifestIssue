62ba294ff2bef0dfb43cb24d4d11cc42800c83e6
Carmen Alvarez <calvarez@libon.com>
2018-10-26 17:57:32 +0200

    initial commit

---
+++ app/src/test/java/example/com/robolectricmigration/ExampleUnitTest.java
@@ -0,0 +1,16 @@
+package example.com.robolectricmigration;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(RobolectricTestRunner.class)
+public class ExampleUnitTest {
+    @Test
+    public void exampleTest() {
+        assertTrue(true);
+    }
+}
